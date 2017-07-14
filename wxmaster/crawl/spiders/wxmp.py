289072a6f05a303a9b877c8460b2f33a66b00cb4
BaoQiang <mailbaoqiang@gmail.com>
2017-07-14 15:48:52 +0800

    sogou spider

--- wxmaster/crawl/spiders/wxmp.py
+++ wxmaster/crawl/spiders/wxmp.py
@@ -8,32 +8,46 @@
 @time: 2017/5/26 16:07
 """
 
-from scrapy.spiders import Rule, CrawlSpider
-from scrapy.linkextractors import LinkExtractor
-from wxmaster.pth import FILE_PATH
-from wxmaster.crawl.items import WxMpItem
 import re
 import time
-import base64
-import requests
 import traceback
 from datetime import datetime
+from urllib.parse import unquote
+
+import requests
+from scrapy import Request
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import Rule, CrawlSpider
+
+from wxmaster.crawl.items import WxMpItem
+from wxmaster.pth import FILE_PATH
 
 num_pat = re.compile('[\d]+')
 
 input_file = '{}/words.txt'.format(FILE_PATH)
 out_file = '{}/sogoump.json'.format(FILE_PATH)
 
+root_url = 'http://weixin.sogou.com'
+
+time_fmt = "%Y-%m-%d %H:%M:%S"
+
+
+# time_fmt = "%Y-%m-%d"
 
 class SogouMpSpider(CrawlSpider):
     name = 'sogoump_spider'
 
-    def __init__(self, *a, **kw):
-        super().__init__(*a, **kw)
-        self.start_urls = get_search_words()
+    def start_requests(self):
+        return [Request(root_url, callback=self.parse_list)]
+
+    def parse_list(self):
+        urls = get_search_words()
+
+        for url in urls:
+            yield Request(url, callback=self.parse_item)
 
     rules = [
-        Rule(LinkExtractor(allow=('/weixin.*query.*',)), callback='parse_item', follow=False,
+        Rule(LinkExtractor(allow=('/weixin.*query.*',)), callback='parse_item', follow=True,
              process_request='add_header')
     ]
 
@@ -42,44 +56,73 @@ class SogouMpSpider(CrawlSpider):
         return request
 
     def parse_item(self, response):
-        mp = WxMpItem()
+        month_url = get_month_url(response.body.decode())
+        if month_url:
+            try:
+                json_data = requests.get(month_url).json()
+                msg_dic = json_data['msg']
+            except:
+                msg_dic = {}
+        else:
+            msg_dic = {}
+
+        mp_list = []
         classes = response.selector.xpath('//ul[@class="news-list2"]/li')
         for item in classes:
+            mp = WxMpItem()
             try:
                 mp['name'] = ''.join([i.extract().strip() for i in item.xpath('.//p[@class="tit"]//text()')])
                 mp['uid'] = item.xpath('.//p[@class="info"]/label/text()')[0].extract().strip()
 
-                mp['month_cnt'] = ''.join([i.extract().strip() for i in item.xpath('.//p[@class="info"]/text()')])
-                mp['month_cnt'] = trim_num(mp['month_cnt'])
+                # 二维码
                 # mp['qrcode'] = item.xpath('.//div[@class="ew-pop"]/span/img/@src')[0].extract().strip()
-                mp['intro'] = ''.join([i.extract().strip() for i in item.xpath('.//dl[1]/dd//text()')])
 
-                mp['upt_time'] = ''.join([i.extract().strip() for i in item.xpath('.//dl[2]/dd/span/text()')])
-                mp['upt_time'] = trim_time(mp['upt_time'])
+                # 简介
+                mp['desc'] = ''.join([i.extract().strip() for i in item.xpath('.//dl[1]/dd//text()')])
+
+                # 公司名称
+                company_url = ''.join([i.extract().strip() for i in item.xpath('.//dl[2]/dt//text()')])
+                if company_url and '认证' in company_url:
+                    mp['company'] = ''.join([i.extract().strip() for i in item.xpath('.//dl[2]/dd//text()')])
+                else:
+                    mp['company'] = ''
+
+                # 最新文章
+                mp['latest_article'] = ''.join([i.extract().strip() for i in item.xpath('.//dl[last()]/dd/a//text()')])
+
+                # 更新时间
+                upt_url = ''.join([i.extract().strip() for i in item.xpath('.//dl[last()]/dd/span//text()')])
+                mp['upt_time'] = get_upt_time(upt_url)
                 mp['spider_time'] = get_current()
 
-                mp['url'] = response.url
+                # 阅读总数
+                li_url = item.xpath('./@d')[0].extract().strip()
+                mp['month_cnt'] = int(msg_dic.get(li_url, '0,0').split(',')[0])
+                mp['read_cnt'] = int(msg_dic.get(li_url, '0,0').split(',')[1])
+
+                mp['url'] = unquote(response.url)
 
             except Exception as e:
                 traceback.print_exc()
 
-            with open(out_file, 'a', encoding='utf-8') as fw:
+            mp_list.append(mp)
+
+        # 保存数据
+        with open(out_file, 'a', encoding='utf-8') as fw:
+            for mp in mp_list:
                 fw.write('{}\n'.format(mp))
 
 
-search_fmt = 'http://weixin.sogou.com/weixin?query={}&type=1&page=1'
-
-headers = {
-    'Cookie': 'pgv_pvi=1520665600; RK=hf0PpTkeaa; pac_uid=1_779439458; pt2gguin=o0779439458; ptcz=da82dcae41f1c11792f5602d8a39d0447963e6124b8c380525c6e567ebd8ff03; noticeLoginFlag=1; remember_acct=xiaoege01%40gmail.com; pgv_pvid=3203998305; o_cookie=779439458; dm_login_weixin_scan='
-}
+def get_month_url(url):
+    m = month_pat.search(url)
+    if m:
+        return '{}{}'.format(root_url, m.group(1))
+    else:
+        return None
 
 
 def get_headers():
     return headers
-
-
-# time_fmt = "%Y-%m-%d %H:%M:%S"
-time_fmt = "%Y-%m-%d"
 
 
 def get_current():
@@ -95,10 +138,21 @@ def trim_num(text):
 
 
 def trim_time(time):
-    if '-' in time:
+    if not time:
+        return ''
+    elif '-' in time:
         return time
     else:
         return get_current()
+
+
+def get_upt_time(src):
+    m = upt_pat.search(src)
+    if m:
+        upt_time = time.localtime(int(m.group(1)))
+        return time.strftime(time_fmt, upt_time)
+    else:
+        return ''
 
 
 def get_search_words():
@@ -107,8 +161,27 @@ def get_search_words():
         return [search_fmt.format(line) for line in lines]
 
 
-def main():
+search_fmt = 'http://weixin.sogou.com/weixin?query={}&type=1&page=1'
+
+headers = {
+    'Cookie': 'pgv_pvi=1520665600; RK=hf0PpTkeaa; pac_uid=1_779439458; pt2gguin=o0779439458; ptcz=da82dcae41f1c11792f5602d8a39d0447963e6124b8c380525c6e567ebd8ff03; noticeLoginFlag=1; remember_acct=xiaoege01%40gmail.com; pgv_pvid=3203998305; o_cookie=779439458; dm_login_weixin_scan='
+}
+
+month_pat = re.compile('account_anti_url[ =]+?"(.+)?"')
+upt_pat = re.compile('(\d)+')
+
+
+def tmp1():
     print(time.time())
+
+
+def tmp():
+    s = 'b326815a'
+    print(type(upt_pat.search(s).group()))
+
+
+def main():
+    tmp()
 
 
 if __name__ == '__main__':
